e1dc5992584b766899670e46bf214ade231b0f49
Isk-On <119677180+Isk-On@users.noreply.github.com>
2024-11-11 10:21:36 +0500

    Update server.js

--- server.js
+++ server.js
@@ -98,6 +98,6 @@ app.post('/login', (req, res) => {
 });
 
 // Запуск сервера
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Сервер работает на порту ${PORT}`);
 });
